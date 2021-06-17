35d34e75398d7dce388f069b017dbf07c6143a09
Abhishek <Abhishek@MSI>
2021-06-18 02:52:16 +0530

    Autodesk changes

---
+++ com.crm.autodesk/src/main/java/AutodeskSelenium/com/crm/autodesk/App.java
@@ -0,0 +1,13 @@
+package AutodeskSelenium.com.crm.autodesk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.crm.autodesk/src/test/java/AutodeskSelenium/com/crm/autodesk/sampleClass.java
@@ -0,0 +1,18 @@
+package AutodeskSelenium.com.crm.autodesk;
+
+import org.apache.poi.util.SystemOutLogger;
+import org.testng.annotations.Test;
+
+public class sampleClass {
+
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("Hi");
+		String USER= System.getProperty("Hi");
+		String BROWSER=System.getProperty("Browser");
+		
+		System.out.println(USER);
+		System.out.println(BROWSER);
+	}
+}
